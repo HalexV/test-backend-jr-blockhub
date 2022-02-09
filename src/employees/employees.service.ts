7662e1d94bba45f21e8db848b536dc8514aab6ed
Halex Viotto Gomes <halexx002@gmail.com>
2022-02-09 16:44:15 -0400

    feat: create employee

---
+++ src/employees/employees.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateEmployeeDto } from './dto/create-employee.dto';
+import { UpdateEmployeeDto } from './dto/update-employee.dto';
+import { Employee, EmployeeDocument } from './entities/employee.schema';
+
+@Injectable()
+export class EmployeesService {
+  constructor(
+    @InjectModel(Employee.name) private employeeModel: Model<EmployeeDocument>,
+  ) {}
+
+  async create(createEmployeeDto: CreateEmployeeDto): Promise<Employee> {
+    const employee = new this.employeeModel(createEmployeeDto);
+    return await employee.save();
+  }
+}
