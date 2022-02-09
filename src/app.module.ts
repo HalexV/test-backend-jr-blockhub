55fa81919305d82b1659df02a76c5bd21575f925
Halex Viotto Gomes <halexx002@gmail.com>
2022-02-09 16:32:54 -0400

    feat: add mongoose to module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { EmployeesModule } from './employees/employees.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot('mongodb://mongodb/projects-manager'),
+    EmployeesModule,
+  ],
 })
 export class AppModule {}
