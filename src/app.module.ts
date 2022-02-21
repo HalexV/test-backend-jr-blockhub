421de36bb3cf29bcb816945fe72ed6184dd94fb0
Halex Viotto Gomes <halexx002@gmail.com>
2022-02-21 14:34:03 -0400

    feat: add projects module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
 import { EmployeesModule } from './employees/employees.module';
+import { ProjectsModule } from './projects/projects.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://mongodb/projects-manager'),
     EmployeesModule,
+    ProjectsModule,
   ],
 })
 export class AppModule {}
