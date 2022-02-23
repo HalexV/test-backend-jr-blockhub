ac8dc89a691a0873102333bbf730bad94a2a6dd0
Halex Viotto Gomes <halexx002@gmail.com>
2022-02-23 09:08:47 -0400

    test: /projects post should return 201 when creating with valid data

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,23 +2,57 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { Connection } from 'mongoose';
+import { DatabaseService } from './../src/database/database.service';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let dbConnection: Connection;
+  let httpServer;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
+    dbConnection = moduleFixture
+      .get<DatabaseService>(DatabaseService)
+      .getDbHandle();
+    httpServer = app.getHttpServer();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  beforeEach(async () => {
+    await dbConnection.collection('projects').deleteMany({});
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  describe('Projects Resource', () => {
+    describe('/projects (POST)', () => {
+      it('should return 201 when creating with valid data', async () => {
+        const inputPayload = {
+          name: 'test',
+          description: 'description test',
+          startDate: '2022-02-22',
+        };
+
+        const partialExpected = {
+          name: 'test',
+          description: 'description test',
+          startDate: new Date('2022-02-22').toISOString(),
+        };
+
+        const response = await request(httpServer)
+          .post('/projects')
+          .send(inputPayload);
+
+        expect(response.status).toBe(201);
+        expect(response.body).toMatchObject(partialExpected);
+      });
+    });
   });
 });
