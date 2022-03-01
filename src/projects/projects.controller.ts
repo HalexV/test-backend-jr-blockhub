4df3a555bd02e757d9651b45676d6a20dd359e52
Halex Viotto Gomes <halexx002@gmail.com>
2022-03-01 19:05:04 -0400

    feat: get projects should return 200 when listing the projects

--- src/projects/projects.controller.ts
+++ src/projects/projects.controller.ts
@@ -35,8 +35,8 @@ export class ProjectsController {
   }
 
   @Get()
-  findAll() {
-    return this.projectsService.findAll();
+  async findAll() {
+    return await this.projectsService.findAll();
   }
 
   @Get(':id')
