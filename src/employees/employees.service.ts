400121cfcdeb0ff9c770d6c556c29f5faa6bb922
Halex Viotto Gomes <halexx002@gmail.com>
2022-04-09 11:08:29 -0400

    feat: start to use EmployeesMongoRepository

--- src/employees/employees.service.ts
+++ src/employees/employees.service.ts
@@ -1,35 +1,36 @@
-import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Inject, Injectable } from '@nestjs/common';
 import { CreateEmployeeDto } from './dto/create-employee.dto';
 import { UpdateEmployeeDto } from './dto/update-employee.dto';
-import { Employee, EmployeeDocument } from './entities/employee.schema';
+import { Employee } from './entities/employee.schema';
+import { EmployeesMongoRepository } from './repositories/employees.mongo.repository';
+import { IEmployeesRepository } from './repositories/interface.employees.repository';
 
 @Injectable()
 export class EmployeesService {
   constructor(
-    @InjectModel(Employee.name) private employeeModel: Model<EmployeeDocument>,
+    @Inject(EmployeesMongoRepository)
+    private readonly projectsRepository: IEmployeesRepository,
   ) {}
 
-  async create(createEmployeeDto: CreateEmployeeDto): Promise<Employee> {
-    const employee = new this.employeeModel(createEmployeeDto);
-    return await employee.save();
-  }
+  // async create(createEmployeeDto: CreateEmployeeDto): Promise<Employee> {
+  //   const employee = new this.employeeModel(createEmployeeDto);
+  //   return await employee.save();
+  // }
 
-  async update(
-    id: string,
-    updateEmployeeDto: UpdateEmployeeDto,
-  ): Promise<Employee> {
-    return await this.employeeModel.findByIdAndUpdate(id, updateEmployeeDto, {
-      returnDocument: 'after',
-    });
-  }
+  // async update(
+  //   id: string,
+  //   updateEmployeeDto: UpdateEmployeeDto,
+  // ): Promise<Employee> {
+  //   return await this.employeeModel.findByIdAndUpdate(id, updateEmployeeDto, {
+  //     returnDocument: 'after',
+  //   });
+  // }
 
-  async findOne(id: string): Promise<Employee> {
-    return await this.employeeModel.findById(id);
-  }
+  // async findOne(id: string): Promise<Employee> {
+  //   return await this.employeeModel.findById(id);
+  // }
 
-  async findAll(): Promise<Employee[]> {
-    return await this.employeeModel.find();
-  }
+  // async findAll(): Promise<Employee[]> {
+  //   return await this.employeeModel.find();
+  // }
 }
