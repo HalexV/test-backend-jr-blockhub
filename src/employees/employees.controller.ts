a9826193714170023e5e53b8be57a1977d64c154
Halex Viotto Gomes <halexx002@gmail.com>
2022-04-09 11:10:46 -0400

    chore: comment all routes because of the start implementation of employees repository

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -16,50 +16,50 @@ import { UpdateEmployeeDto } from './dto/update-employee.dto';
 export class EmployeesController {
   constructor(private readonly employeesService: EmployeesService) {}
 
-  @Post()
-  async create(@Body() createEmployeeDto: CreateEmployeeDto) {
-    try {
-      return await this.employeesService.create(createEmployeeDto);
-    } catch (error) {
-      if (error.name === 'ValidationError') {
-        throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
-      }
+  // @Post()
+  // async create(@Body() createEmployeeDto: CreateEmployeeDto) {
+  //   try {
+  //     return await this.employeesService.create(createEmployeeDto);
+  //   } catch (error) {
+  //     if (error.name === 'ValidationError') {
+  //       throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
+  //     }
 
-      throw error;
-    }
-  }
+  //     throw error;
+  //   }
+  // }
 
-  @Patch(':id')
-  async update(
-    @Param('id') id: string,
-    @Body() updateEmployeeDto: UpdateEmployeeDto,
-  ) {
-    try {
-      return await this.employeesService.update(id, updateEmployeeDto);
-    } catch (error) {
-      if (error.name === 'ValidationError') {
-        throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
-      }
+  // @Patch(':id')
+  // async update(
+  //   @Param('id') id: string,
+  //   @Body() updateEmployeeDto: UpdateEmployeeDto,
+  // ) {
+  //   try {
+  //     return await this.employeesService.update(id, updateEmployeeDto);
+  //   } catch (error) {
+  //     if (error.name === 'ValidationError') {
+  //       throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
+  //     }
 
-      throw error;
-    }
-  }
+  //     throw error;
+  //   }
+  // }
 
-  @Get('all')
-  async listEmployees() {
-    try {
-      return await this.employeesService.findAll();
-    } catch (error) {
-      throw error;
-    }
-  }
+  // @Get('all')
+  // async listEmployees() {
+  //   try {
+  //     return await this.employeesService.findAll();
+  //   } catch (error) {
+  //     throw error;
+  //   }
+  // }
 
-  @Get(':id')
-  async listEmployee(@Param('id') id: string) {
-    try {
-      return await this.employeesService.findOne(id);
-    } catch (error) {
-      throw error;
-    }
-  }
+  // @Get(':id')
+  // async listEmployee(@Param('id') id: string) {
+  //   try {
+  //     return await this.employeesService.findOne(id);
+  //   } catch (error) {
+  //     throw error;
+  //   }
+  // }
 }
