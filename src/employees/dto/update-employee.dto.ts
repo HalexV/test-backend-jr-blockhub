95557ddd1e586d4b4fb9bc49194681023dd07084
Halex Viotto Gomes <halexx002@gmail.com>
2022-04-09 11:05:47 -0400

    feat: add projects field and remove explicit  admission field

--- src/employees/dto/update-employee.dto.ts
+++ src/employees/dto/update-employee.dto.ts
@@ -2,6 +2,6 @@ import { PartialType } from '@nestjs/mapped-types';
 import { CreateEmployeeDto } from './create-employee.dto';
 
 export class UpdateEmployeeDto extends PartialType(CreateEmployeeDto) {
-  admission?: Date;
   active?: boolean;
+  projects?: string[];
 }
