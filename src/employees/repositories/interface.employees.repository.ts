4dfc381ae328ed2abd460b234a684f91523c640e
Halex Viotto Gomes <halexx002@gmail.com>
2022-04-11 10:29:33 -0400

    chore: add new function return option

--- src/employees/repositories/interface.employees.repository.ts
+++ src/employees/repositories/interface.employees.repository.ts
@@ -5,7 +5,7 @@ import { Employee } from '../entities/employee.schema';
 export interface IEmployeesRepository {
   create(employee: CreateEmployeeDto): Promise<Employee>;
   update(id: string, updateEmployeeDto: UpdateEmployeeDto): Promise<Employee>;
-  findOneByName(name: string): Promise<Employee>;
-  findById(id: string): Promise<Employee>;
+  findOneByName(name: string): Promise<Employee> | undefined;
+  findById(id: string): Promise<Employee> | undefined;
   findAll(): Promise<Employee[]>;
 }
