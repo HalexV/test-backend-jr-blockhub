e0bf8538f99be76af1c635eb69e56792571727e8
Halex Viotto Gomes <halexx002@gmail.com>
2022-05-03 15:58:28 -0400

    feat: import ProjectsModule

--- src/employees/employees.module.ts
+++ src/employees/employees.module.ts
@@ -4,12 +4,14 @@ import { EmployeesController } from './employees.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { EmployeeSchema, Employee } from './entities/employee.schema';
 import { EmployeesMongoRepository } from './repositories/employees.mongo.repository';
+import { ProjectsModule } from '../projects/projects.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: Employee.name, schema: EmployeeSchema },
     ]),
+    ProjectsModule,
   ],
   controllers: [EmployeesController],
   providers: [EmployeesService, EmployeesMongoRepository],
