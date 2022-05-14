0d384fe35e701253f1a5ce35d997bf71f5b6e16a
Halex Viotto Gomes <halexx002@gmail.com>
2022-05-14 18:12:42 -0400

    feat: remove __v from object

--- src/database/utils/mongo/MongoHelper.ts
+++ src/database/utils/mongo/MongoHelper.ts
@@ -1,6 +1,7 @@
 export default class MongoHelper {
   static map(document: any): any {
     const { _id, ...documentWithoutId } = document;
+    Reflect.deleteProperty(documentWithoutId, '__v');
     return Object.assign({}, documentWithoutId, { id: _id });
   }
 }
