38e6e6210235585b517d3b5fc49b19311256336f
Halex Viotto Gomes <halexx002@gmail.com>
2022-05-21 16:39:43 -0400

    feat: add delete method

--- src/employees/repositories/interface.employees.repository.ts
+++ src/employees/repositories/interface.employees.repository.ts
@@ -5,6 +5,7 @@ import { Employee } from '../entities/employee.schema';
 export interface IEmployeesRepository {
   create(employee: CreateEmployeeDto): Promise<Employee>;
   update(id: string, updateEmployeeDto: UpdateEmployeeDto): Promise<Employee>;
+  delete(id: string): Promise<boolean>;
   findOneByName(name: string): Promise<Employee> | undefined;
   findById(id: string): Promise<Employee> | undefined;
   findAll(): Promise<Employee[]>;
